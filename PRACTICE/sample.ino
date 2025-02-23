50a827192a33c6003cddef3444d9347b0f868448
reishimoto <reishimoto423@gmail.com>
2025-02-24 02:03:29 +0900

    テスト

--- PRACTICE/sample.ino
+++ PRACTICE/sample.ino
@@ -1,12 +1,11 @@
+
+
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
-  Serial.println("Hello World");
-
+  pinMode(13, OUTPUT);
 }**J
 
 void loop() {
-    pinMode(13, OUTPUT);
     digitalWrite(13, HIGH);
     delay(1000);
     digitalWrite(13, LOW);
